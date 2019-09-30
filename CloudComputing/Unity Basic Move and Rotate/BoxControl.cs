db21b074cee55f6c2a72eab3b2b35ef1c0aa8a88
jintaeks <jintaeks@gmail.com>
2019-09-30 22:44:36 +0900

    Added C# programming files.

---
+++ CloudComputing/Unity Basic Move and Rotate/BoxControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxControl : MonoBehaviour
+{
+    public float _speed = 10.0f;
+    public float _rotSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 forward = gameObject.transform.forward;
+        forward *= Time.deltaTime * _speed * v;
+        gameObject.transform.position += forward;
+
+        Quaternion rot = new Quaternion();
+        rot.SetAxisAngle(Vector3.up, h * _rotSpeed * Time.deltaTime);
+        gameObject.transform.rotation *= rot;
+    }
+}
---
+++ CsharpProgramming/CSharpApplication/Basic Reference Files/Basic01 Type Conversion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    class KBase
+    {
+    }
+    class KDerived : KBase
+    {
+        public int _iData;
+    }
+
+    static void Main(string[] args)
+    {
+        double d = 5673.74;
+        int i;
+
+        // cast double to int.
+        i = (int)d;
+        Console.WriteLine(i);
+        Console.WriteLine("Hello" + i.ToString());
+
+        KDerived a;
+        KBase b = new KBase();
+        //a = (KDerived)b; // exception
+        a = b as KDerived; // a is null
+        Console.ReadKey();
+    }
+}
