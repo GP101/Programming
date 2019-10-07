33d6f52322c30723fe8e76e81e9c69ff51e22e6f
jintaeks <jintaeks@gmail.com>
2019-10-08 01:24:42 +0900

    Added Unity projects.

--- CsharpProgramming/CSharpApplication/Program - Array Lookup Table.cs
+++ CsharpProgramming/CSharpApplication/Program - Array Lookup Table.cs
@@ -11,7 +11,7 @@ class Program
         public ConsoleKey key;
         public string dow;
 
-        public KeyValueInfo(ConsoleKey key_, string dow_)
+        public KeyValueInfo( ConsoleKey key_, string dow_ )
         {
             key = key_;
             dow = dow_;
@@ -20,18 +20,25 @@ class Program
 
     static void Main( string[] args )
     {
-        int[,] a = new int[2,3] { { 1, 2, 3 },{ 4, 5, 6 } };
-        string[] names = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        int[,] a = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+        string[] names = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
         //KeyValueInfo[] keyValue = new KeyValueInfo[2] { new KeyValueInfo(), new KeyValueInfo() };
         //keyValue[0].key = ConsoleKey.D1;
         //keyValue[0].dow = "Sun";
         //keyValue[1].key = ConsoleKey.D2;
         //keyValue[1].dow = "Mon";
+
         KeyValueInfo[] keyValue = new KeyValueInfo[2] 
         {
-            new KeyValueInfo(ConsoleKey.D1, "Sun"),
-            new KeyValueInfo(ConsoleKey.D2, "Mon")
+            new KeyValueInfo(){ key=ConsoleKey.D1, dow="Sun"},
+            new KeyValueInfo(){ key=ConsoleKey.D2, dow="Mon"}
         };
+
+        //KeyValueInfo[] keyValue = new KeyValueInfo[2]
+        //{
+        //    new KeyValueInfo(ConsoleKey.D1, "Sun"),
+        //    new KeyValueInfo(ConsoleKey.D2, "Mon")
+        //};
 
         ConsoleKeyInfo keyInfo = Console.ReadKey( true );
         //if( keyInfo.Key == ConsoleKey.D1 )
