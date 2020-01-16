246d21deb12b758867d259f4049e87653a61cbe6
jintaeks <jintaeks@gmail.com>
2020-01-17 03:47:41 +0900

    CsharpProgramming: Added covariance example, revised event example, removed old event examples, added collection class example.

--- CsharpProgramming/CSharpApplication/StepByStepFiles/Program26 Event03.cs
+++ CsharpProgramming/CSharpApplication/StepByStepFiles/Program26 Event03.cs
@@ -22,26 +22,30 @@ class Program1
 
     class Subscriber
     {
+        private string _name;
         private Sample _sample;
-        public Subscriber(Sample s)
+        public Subscriber(string name, Sample s)
         {
+            _name = name;
             _sample = s;
-            _sample.hello += Handler;
+            _sample.hello += this.Handler;
         }
         public void Handler(object sender, HelloEventArgs args)
         {
             Console.WriteLine("{0} received event {1} from {2}."
-                , this.ToString(), args.Time, sender.ToString());
+                , _name, args.Time, sender.ToString());
         }
     }
 
     static void Main(string[] args)
     {
         Sample s = new Sample();
-        Subscriber t = new Subscriber(s);
+        Subscriber t = new Subscriber("John", s);
+        Subscriber t2 = new Subscriber("Jane", s);
         s.DoAction();
     }
     /** output:
-        Program1+Subscriber received event 2 from Program1+Sample.
+        John received event 2 from Program1+Sample.
+        Jane received event 2 from Program1+Sample.
     */
 }
