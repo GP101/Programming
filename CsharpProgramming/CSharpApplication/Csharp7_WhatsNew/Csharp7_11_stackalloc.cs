390338cb475b09174b02940ac3805bd91aa64145
jintaeks <jintaeks@gmail.com>
2022-09-02 23:06:11 +0900

    20220902 jintaeks

---
+++ CsharpProgramming/CSharpApplication/Csharp7_WhatsNew/Csharp7_11_stackalloc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 3;
+            Span<int> numbers = stackalloc int[length];
+            for (var i = 0; i < length; i++)
+            {
+                numbers[i] = i;
+            }
+        }
+    }
+}
