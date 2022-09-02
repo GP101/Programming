cc1d1af8d704a817f2525e6e2d48d0f067440a23
SEO JIN TAEK <jintaeks@gmail.com>
2022-09-02 22:48:20 +0900

    20220902_jintaeks

---
+++ CsharpProgramming/CSharpApplication/Csharp7_WhatsNew/Csharp7_09_NamedParameter.cs
@@ -0,0 +1,20 @@
+﻿// https://www.c-sharpcorner.com/article/top-10-new-features-of-c-sharp-7-with-visual-studio-2017/
+using static System.Console;
+using static System.Text.Encoding;
+
+class Program
+{
+    public static int AddNumber(int firstNumber, int secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+
+    static void Main(string[] args)
+    {
+        AddNumber(2, 3);
+        AddNumber(firstNumber: 2, secondNumber: 3);
+        AddNumber(2, secondNumber: 3);
+        AddNumber(firstNumber: 2, 3);
+        AddNumber(secondNumber: 3, firstNumber: 2);
+    }
+}
---
+++ CsharpProgramming/CSharpApplication/Csharp7_WhatsNew/Csharp7_10_DefaultLiteral.cs
@@ -0,0 +1,37 @@
+﻿// https://www.c-sharpcorner.com/article/top-10-new-features-of-c-sharp-7-with-visual-studio-2017/
+using static System.Console;
+using static System.Text.Encoding;
+
+class Program
+{
+    static T[] InitializeArray<T>(int length, T initialValue = default)
+    {
+        if (length < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(length), "Array length must be nonnegative.");
+        }
+
+        var array = new T[length];
+        for (var i = 0; i < length; i++)
+        {
+            array[i] = initialValue;
+        }
+        return array;
+    }
+
+    static void Display<T>(T[] values) => Console.WriteLine($"[ {string.Join(", ", values)} ]");
+
+    static void Main(string[] args)
+    {
+        Display(InitializeArray<int>(3));  // output: [ 0, 0, 0 ]
+        Display(InitializeArray<bool>(4, default));  // output: [ False, False, False, False ]
+
+        System.Numerics.Complex fillValue = default;
+        Display(InitializeArray(3, fillValue));  // output: [ (0, 0), (0, 0), (0, 0) ]
+    }
+    /** output:
+        [ 0, 0, 0 ]
+        [ False, False, False, False ]
+        [ (0, 0), (0, 0), (0, 0) ]
+    */
+}
