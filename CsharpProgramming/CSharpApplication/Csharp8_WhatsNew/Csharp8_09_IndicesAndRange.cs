35e5289e2bfd88af93407bf996517a04df429084
jintaeks <jintaeks@gmail.com>
2022-09-03 22:57:38 +0900

    20220903 jintaeks

--- CsharpProgramming/CSharpApplication/Csharp8_WhatsNew/Csharp8_09_IndicesAndRange.cs
+++ CsharpProgramming/CSharpApplication/Csharp8_WhatsNew/Csharp8_09_IndicesAndRange.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var words = new string[]
+            {
+                // index from start    index from end
+                "The",      // 0                   ^9
+                "quick",    // 1                   ^8
+                "brown",    // 2                   ^7
+                "fox",      // 3                   ^6
+                "jumped",   // 4                   ^5
+                "over",     // 5                   ^4
+                "the",      // 6                   ^3
+                "lazy",     // 7                   ^2
+                "dog"       // 8                   ^1
+            };              // 9 (or words.Length) ^0        }
+            Console.WriteLine($"The last word is {words[^1]}");
+            // writes "dog"
+            var quickBrownFox = words[1..4];
+            foreach (string s in quickBrownFox)
+                WriteLine(s);
+        }
+        /** output:
+            The last word is dog
+            quick
+            brown
+            fox         
+        */
+    }
+}
