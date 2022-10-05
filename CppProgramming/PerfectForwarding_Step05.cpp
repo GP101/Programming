89565618da4cf2313fc187cb03282b05cec6e17a
SEO JIN TAEK <jintaeks@gmail.com>
2022-10-05 09:41:22 +0900

    Perfect Forwarding C++ step by step examples. 20221005_jintaeks

---
+++ CppProgramming/PerfectForwarding_Step05.cpp
@@ -0,0 +1,99 @@
+// PerfectForwarding.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <utility>
+
+class KTest
+{
+private:
+    int* pData;
+    int     dataSize;
+public:
+    KTest()
+    {
+        dataSize = 3;
+        pData = new int[dataSize];
+        printf("constructor\r\n");
+    }
+
+    KTest(const KTest& rhs) // copy constructor
+    {
+        dataSize = rhs.dataSize;
+        pData = new int[dataSize];
+        memcpy(pData, rhs.pData, sizeof(int) * dataSize);
+        printf("copy constructor\r\n");
+    }
+
+    KTest(KTest&& rhs) noexcept// move constructor
+    {
+        dataSize = rhs.dataSize;
+        pData = rhs.pData;
+
+        rhs.dataSize = 0;
+        rhs.pData = nullptr;
+        printf("move constructor\r\n");
+    }
+
+    KTest& operator=(const KTest& rhs)
+    {
+        if (this == &rhs)
+            return *this;
+
+        Release();
+
+        dataSize = rhs.dataSize;
+        pData = new int[dataSize];
+        memcpy(pData, rhs.pData, sizeof(int) * dataSize);
+        printf("operator=()\r\n");
+        return *this;
+    }
+
+    KTest& operator=(KTest&& rhs) noexcept // move assignment operator
+    {
+        if (this == &rhs)
+            return *this;
+
+        Release();
+
+        dataSize = rhs.dataSize;
+        pData = rhs.pData;
+
+        rhs.dataSize = 0;
+        rhs.pData = nullptr;
+
+        printf("move operator=()\r\n");
+        return *this;
+    }
+
+    void Release()
+    {
+        dataSize = 0;
+        if (pData != nullptr)
+            delete[] pData;
+        pData = nullptr;
+    }
+    ~KTest()
+    {
+        Release();
+        printf("destructor\r\n");
+    }
+};
+
+void Test(KTest t)
+{
+    printf("Test()\r\n");
+}
+
+KTest GetTest()
+{
+    KTest t;
+    return t;
+}
+
+void main()
+{
+    KTest t = GetTest();
+}
