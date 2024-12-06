92927f20dcd3f561a21f6e026651227620713e56
jintaeks <jintaeks@gmail.com>
2024-12-06 20:00:32 +0900

    Added jagged array example.

--- CsharpProgramming/CSharpApplication/Basic Reference Files/Basic03 Array and foreach.cs
+++ CsharpProgramming/CSharpApplication/Basic Reference Files/Basic03 Array and foreach.cs
@@ -4,18 +4,26 @@ using System.Text;
 
 class Program
 {
-    static void Main( string[] args )
+    static void Main(string[] args)
     {
-        int[] marks = new int[5]  { 99,  98, 92, 97, 95};
-        int[] marks2 = { 99,  98, 92, 97, 95};
+        int[] marks = new int[5] { 99, 98, 92, 97, 95 };
+        int[] marks2 = { 99, 98, 92, 97, 95 };
         int[,] a = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
 
-        for( int i = 0; i < marks.Length; ++i )
+        for (int i = 0; i < marks.Length; ++i)
         {
-            Console.Write( "marks[{0}]={1}\r\n", i, marks[i] );
+            Console.Write("marks[{0}]={1}\r\n", i, marks[i]);
         }
-        foreach( int j in marks2 )
-            Console.Write( "j={0}\r\n", j );
+        foreach (int j in marks2)
+            Console.Write("j={0}\r\n", j);
+        for (int row = 0; row < a.GetLength(0); ++row) // (3)
+        {
+            for (int col = 0; col < a.GetLength(1); ++col)
+            {
+                Console.Write($"a[{row},{col}]={a[row, col]},");
+                //Console.Write($"a[{row},{col}]={a[row][col]},"); // doesn't work
+            }
+        }
     }
 }
 /*  output:
@@ -29,4 +37,5 @@ class Program
     j=92
     j=97
     j=95
+    a[0,0]=1,a[0,1]=2,a[0,2]=3,a[1,0]=4,a[1,1]=5,a[1,2]=6,
 */
---
+++ CsharpProgramming/CSharpApplication/Basic Reference Files/Basic03-3 - Jagged Array.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    struct KeyValueInfo
+    {
+        public ConsoleKey key;
+        public string dow;
+
+        public KeyValueInfo(ConsoleKey key_, string dow_)
+        {
+            key = key_;
+            dow = dow_;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int baseValue = 0;
+        int[][] a = new int[2][];
+        for (int row = 0; row < a.Length; row++) {
+            a[row] = new int[3] { baseValue + 1, baseValue + 2, baseValue + 3 };
+            baseValue += 3;
+        }
+        for (int row = 0; row < a.Length; row++) {
+            for (int col = 0; col < a[row].Length; col++) {
+                Console.Write("{0},", a[row][col]);
+            }
+            Console.WriteLine();
+        }
+
+        // step1. create 2D jagged array(array of array) itself
+        KeyValueInfo[][] keyValueArray = new KeyValueInfo[2][];
+        for (int row = 0; row < keyValueArray.Length; row++) {
+            keyValueArray[row] = new KeyValueInfo[3];
+        }
+        // step2. create an instance for array entry
+        for (int row = 0; row < keyValueArray.Length; row++) {
+            for (int col = 0; col < keyValueArray[row].Length; col++) {
+                keyValueArray[row][col] = new KeyValueInfo(ConsoleKey.C, "dummy");
+            }
+        }
+    }
+}
