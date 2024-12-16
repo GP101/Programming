97a12c15e47b63ce4fec9ac7afc1ef1b339bf1e1
jintaeks <jintaeks@gmail.com>
2024-12-16 16:09:56 +0900

    Added Unity framework files.

---
+++ CsharpProgramming/CSharpApplication/UnityFrameworkFiles/Program GameObject01 inherits interfaces.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingUnityEngine
+{
+    public class Component
+    {
+    }
+
+    public class Behavior : Component
+    {
+    }
+
+    public abstract class MonoBehavior : Behavior
+    {
+        public List<Component> m_components;
+        public MonoBehavior()
+        {
+            m_components = new List<Component>();
+        }
+        public T GetComponent<T>() where T : Component
+        {
+            foreach (var comp in m_components)
+            {
+                if (comp.GetType() == typeof(T))
+                {
+                    return (T)comp;
+                }
+            }
+            return default(T);
+        }
+    }
+
+    public interface IBoxCollider
+    {
+        void GetCollider();
+    }
+    public interface IMeshRenderer
+    {
+        void GetMaterial();
+    }
+
+    public class Base
+    {
+    }
+
+    public class GameObject : Base, IBoxCollider, IMeshRenderer
+    {
+        public void GetCollider() { }
+        public void GetMaterial() { }
+
+        // Use this for initialization
+        public void Start()
+        {
+        }
+
+        public void Update()
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameObject e = new GameObject();
+            e.Start();
+            e.Update();
+        }
+    }
+}
